d107c40788726725f9ddb70c86bcaf8b6b709239
Kaiett <nguyenanhkiet845@gmail.com>
2025-04-19 00:32:43 +0700

    update

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,14 +3,15 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDhDuuYfgAKrS8ZhG2fXycY-27jLi-pJeg",
-    authDomain: "fall-detection-9dda4.firebaseapp.com",
-    projectId: "fall-detection-9dda4",
-    storageBucket: "fall-detection-9dda4.firebasestorage.app",
-    messagingSenderId: "930392736385",
-    appId: "1:930392736385:web:2c2df2692b767fae4d4380",
-    measurementId: "G-YR4WVD6FCH",
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import SignIn from './auth/signin.jsx'
 import './App.css'
-import Home from './pages/home.jsx'
+import Dashboard from './pages/Home.jsx'
 import TestData from './pages/TestData.jsx'
 import VoiceAssistant from './VoiceAssistant.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Dashboard />} />
         <Route path="/auth" element={<SignIn />} />
         <Route path="/test" element={<TestData />} />
         <Route path="/voice" element={<VoiceAssistant />} />
